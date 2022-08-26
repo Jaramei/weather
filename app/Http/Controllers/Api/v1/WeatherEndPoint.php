12e412877ce965a8c92cf8e2f2e73dee313357a9
jakub.jablonski <jakubjablonski@pasadema.com>
2022-08-26 10:55:46 +0200

    Add Class API GetCity to get temperature

--- app/Http/Controllers/Api/v1/WeatherEndPoint.php
+++ app/Http/Controllers/Api/v1/WeatherEndPoint.php
@@ -6,10 +6,23 @@ use App\Http\Controllers\Api\ApiController;
 use App\Http\Resources\WeatherResource;
 use App\Models\Weather as WeatherModel;
 use \Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Visualcrossing\Weather\Client;
 
 
 class WeatherEndPoint extends ApiController
 {
+    /**
+     * @var
+     */
+    protected Client $client;
+
+    public function __construct(Client $client) {
+
+        $this->client = $client;
+
+    }
+
     /**
      * @return JsonResponse
      */
@@ -18,4 +31,19 @@ class WeatherEndPoint extends ApiController
     {
         return $this->sendResponse(new WeatherResource(WeatherModel::all()), 'Retrieved Successfully.');
     }
+
+    /**
+     * @return JsonResponse
+     */
+
+    public function getCity(Request $request): JsonResponse
+    {
+
+        $response = $this->client->make()->to($request->city);
+        $temperature = $response->addQuery(['include'=>'current'])->get();
+
+        return $this->sendResponse($temperature,'200');
+
+    }
+
 }
