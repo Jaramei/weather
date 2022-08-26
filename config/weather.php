130c3844a5bfa8c7454f45f5799f71927df2978e
jakub.jablonski <jakubjablonski@pasadema.com>
2022-08-26 09:35:09 +0200

    Change Weather Configuration

--- config/weather.php
+++ config/weather.php
@@ -1,6 +1,6 @@
 <?php
     return [
-        'apiKey' =>'ETAM6MQU48R93CBLMCTWNMMW8',
+        'apiKey' =>'',
         'baseUrl' =>'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline',
         'lang'=>''
     ];
