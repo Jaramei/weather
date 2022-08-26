0f057aec2e215fb96d33896f630f78a735609469
jakub.jablonski <jakubjablonski@pasadema.com>
2022-08-26 09:25:39 +0200

    Clear Config File

--- config/weather.php
+++ config/weather.php
@@ -1,10 +1,6 @@
 <?php
-
     return [
-
-        /** Get Unique API Key from Visualcrossing Platform */
         'apiKey' =>'ETAM6MQU48R93CBLMCTWNMMW8',
         'baseUrl' =>'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline',
         'lang'=>''
-
     ];
